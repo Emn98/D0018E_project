d53edcc48b74423a7a845108863905174c3d8b5a
ItzCornflakez <dikesalex@gmail.com>
2021-11-08 16:58:52 +0100

    add index

---
+++ src/index.php
@@ -0,0 +1,24 @@
+<!-- This will serve as the main page for our e-comerce site offbrand.pwr-->
+<?php 
+  session_start();
+?>
+<!DOCTYPE html>
+  <html lang="sv">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/main_page.css">
+    <title>Offbrand.pwr</title>
+  </head>
+  <body>
+    <header>
+      <h1>OFF<span>BRAND</span></h1>
+      <nav>
+        <ul class="menu">
+          <li><a href="Profiles/my_site_director.php">Min sida</a></li>
+          <li><a href="#">kundvagn</a></li>
+        </ul>
+      </nav>
+    </header>
+  </body>
+</html>
