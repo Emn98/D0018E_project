7879d07fab0f14c2bffd3a253c2f2c3f7a68ab51
ItzCornflakez <dikesalex@gmail.com>
2021-11-09 10:05:01 +0100

    removed global, moving files into src

--- src/database.php
+++ src/database.php
@@ -5,7 +5,6 @@ $username = "phpmyadmin";
 $password = "Offbrand123$";
 $database = "Website";
 
-global $conn;
 $conn; = new mysqli($host,$username,$password, $database);
 
 // error check for connection
