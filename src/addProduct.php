50dea87dbe47c75e1b7dc3a36711f394372b62f1
ItzCornflakez <dikesalex@gmail.com>
2021-11-09 08:58:16 +0100

    ap 2

--- src/addProduct.php
+++ src/addProduct.php
@@ -42,8 +42,16 @@
         }
         echo "Connected successfully";
 
-         $query = "INSERT INTO Website.Products (productName, productDescription, categoryID, quantity, price, size, color, discount, picture)
-         VALUES ("$productName", "$productDescription", $category, $quantity, $price, $size, $color, $discount, "$picture")";
+        /*$query = "INSERT INTO Website.Products (productName, productDescription, categoryID, quantity, price, size, color, discount, picture)
+         VALUES ('$productName', '$productDescription', $category, $quantity, $price, $size, $color, $discount, '$picture')";
+        */
+        $query = "CREATE TABLE MyGuests (
+          id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+          firstname VARCHAR(30) NOT NULL,
+          lastname VARCHAR(30) NOT NULL,
+          email VARCHAR(50),
+          reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+          )";
         
 
         // perform query
