34667697bbfa0e97fbc0fd3d330b697abc85b011
Emn98 <emilnyberg1998@gmail.com>
2021-11-09 10:48:51 +0100

    what is this even xd

--- src/Accounts/login_page.php
+++ src/Accounts/login_page.php
@@ -1,4 +1,16 @@
 <!-- This is the log in page for our e-comerce site -->
+<?php 
+    if(isset($_POST["email"]) and isset($_POST["password"])){
+        if(($_POST["email"]!= "") and $_POST["password"]!=""){
+            header("Location: my_page.php");
+            exit;
+        }else{
+            echo '<script type="text/javascript">',
+                 'login_error_pop_up();',
+                 '</script>';
+        }
+    }
+?>
 <!DOCTYPE html>
   <html lang="en">
   <head>
@@ -11,7 +23,7 @@
     <div class="login_box">
         <h1>Sign in</h1>
         <div class="login_form_box">
-            <form class="login_form" method="POST" action="my_page.php">
+            <form class="login_form" method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
                 <h2 class="login_inp_text">Enter Email</h2>
                 <input type="text" class="login_inp" name="email">
                 <br>
@@ -19,9 +31,14 @@
                 <input type="text" class="login_inp" name="password">
                 <br>
                 <button type="submit" class="btn">Login</button>
-                <p class="registration_link">Don't have an account? <a href="registration_page.php">Register here</a></p>
+                <p class="registration_link">Don't have an account? <a href="/registration_page.php">Register here</a></p>
             </form>
         </div>
     </div>
   </body>
 </html>
+<script type="text/javascript">
+    function login_error_pop_up(){
+        alert("Test")
+    }
+</script>
