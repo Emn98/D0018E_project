69d62035e193140cb67fe42e94ac80a325a587c8
Emn98 <emilnyberg1998@gmail.com>
2021-11-10 10:40:04 +0100

    trying stuff

--- src/Accounts/my_page.php
+++ src/Accounts/my_page.php
@@ -23,10 +23,6 @@
             $input_email = $_POST["email"];
             $input_pwd = $_POST["password"];
             $hashed_pwd = sha1($input_pwd);
-
-            echo $hashed_pwd;
-            echo $input_pwd;
-            echo "<br>";
     
             $query = $con->prepare("SELECT first_name, last_name, t_number, addres, user_id FROM USERS WHERE email_addres=? and pwd=?");
             $query->bind_param("ss", $input_email, $hashed_pwd);
@@ -35,43 +31,33 @@
             $query->fetch();
             $query->close();
 
-
-            echo $first_name; 
-            echo $last_name; 
-            echo $tel_nr; 
-            echo $address;
-            echo $user_id;
-
-            //if user_id exists then the user exists in the database
+            //if user_id is set then the user exists in the database
             if(isset($user_id)){
-                $_SESSION["user_id"] = $user_id;//The user will now be seen as logged in. 
-                drawPageLayout($first_name, $last_name, $tel_nr, $address, $user_id, $input_email);
+                $_SESSION["user_id"] = $user_id;//The user will now be seen as logged in.
+                $_SESSION["first_name"] = $first_name;
+                $_SESSION["last_name"] = $last_name;
+                $_SESSION["t_number"]  = $tel_nr;
+                $_SESSION["address"] = $address;
+                $_SESSION["email_address"] = $input_email;
+                $_SESSION["pwd"] = $hashed_pwd;
+                drawPageLayout();
             }else{
                 //If user_id dosen't exists then the authentication failed. Display this to the user.
-                echo "<br>";
-                echo "user don't exist";
                 echo "<div class='auth_failed_container'>";
                 echo "<div class='auth_failed_container_text'>";
-                echo "Wrong account information";
+                echo "The account information provided does not exist";
                 echo "<br>";
                 echo "</div>";
                 echo "<a href='login_page.php'>try again</a>";
                 echo "</div>";
             }
-        }else{
-            $query = $con->prepare("SELECT first_name, last_name, email_addres, t_number, addres FROM USERS WHERE user_id=?");
-            $query->bind_param("s", $_SESSION["user_id"]);
-            $query->execute();
-            $query->bind_result($first_name, $last_name, $email, $tel_nr, $address);
-            $query->fetch();
-            $query->close();
-
-            drawPageLayout($first_name, $last_name, $tel_nr, $address, $_SESSION["user_id"], $email);
         }
-
-        function drawPageLayout($first_name, $last_name, $tel_nr, $address, $user_id, $email){
+        function drawPageLayout(){
             //If the current user is logged in as admin, draw the admin page.
-            if($user_id == 0){
+            if($_SESSION["user_id"] == 0){
+                echo "<br>";
+                echo "you are the admin";
+
             }else{
 
             }
