705e979f6eb4bef8b2f830011013719689b938b4
Chefenz <pingispontus@hotmail.com>
2021-11-11 11:05:04 +0100

    email_exist test 16

--- src/registration_page.php
+++ src/registration_page.php
@@ -135,14 +135,15 @@ if (isset($_POST['submit'])) {
         $query -> bind_param("sssiss", $first_name, $last_name, $email_addres, $t_number, $addres, $sha_pwd);
         $query -> execute();
 
-        $email_exist = $con->prepare("SELECT email_addres FROM USERS WHERE `email_addres` = '$email_addres'");
-        $email_exist->bind_param("s", $email_addres);
-        $email_exist->execute();
-        $result = $con->get_result();
-        $con->close();
+        $email_exist = "SELECT email_addres FROM USERS WHERE email_addres = '$email_addres'");
+        $result = $con->query($email_adders);
+        //$email_exist->bind_param("s", $email_addres);
+        //$email_exist->execute();
+        //$result = $con->get_result();
+        //$con->close();
         //$email_exist = $con->affected_rows;
 
-        if (!($result)) {  //fastnar här
+        if ($result->num_rows > 0) {  //fastnar här
             echo "Email already in use";
         }
         else{
