da605c37d1b6604cbdf7ecb41087e775f843506e
Emil Nyberg <emilnyberg1998@gmail.com>
2021-11-18 14:15:22 +0100

    added city, postal code and C/O addess to my page

--- src/Accounts/my_page.php
+++ src/Accounts/my_page.php
@@ -21,37 +21,37 @@
             $input_pwd = $_POST["password"];
             $hashed_pwd = sha1($input_pwd);
     
-            $query = $con->prepare("SELECT first_name, last_name, t_number, addres, user_id FROM USERS WHERE email_addres=? and pwd=?");
+            $query = $con->prepare("SELECT first_name, last_name, t_number, address_1, address_2, city, postal_code, user_id FROM USERS WHERE email_address=? and pwd=?");
             $query->bind_param("ss", $input_email, $hashed_pwd);
             $query->execute();
-            $query->bind_result($first_name, $last_name, $tel_nr, $address, $user_id);
+            $query->bind_result($first_name, $last_name, $tel_nr, $address_1, $address_2, $city, $postal_code, $user_id);
             $query->fetch();
             $query->close();
 
             //if user_id is set then the user exists in the database
             if(isset($user_id)){
                 $_SESSION["user_id"] = $user_id;//The user will now be seen as logged in.
-                drawPageLayout($first_name, $last_name, $tel_nr, $address, $input_email);
+                drawPageLayout($first_name, $last_name, $tel_nr, $address_1, $address_2, $postal_code, $city, $input_email);
             }else{
                 //If user_id dosen't exists then the authentication failed. Display this to the user.
                 echo "<div class='auth_failed_container'>";
                 echo "<h1>The account information provided does not exist</h1>";
                 echo "<br>";
-                echo "<a href='login_page.php'>try again</a>";
+                echo "<a href='/Accounts/login_page.php'>try again</a>";
                 echo "</div>";
             }
         }else{//If customer already logged in, get the right information. 
-            $query = $con->prepare("SELECT first_name, last_name, email_addres, t_number, addres FROM USERS WHERE user_id=?");
+            $query = $con->prepare("SELECT first_name, last_name, email_address, t_number, address_1, address_2, city, postal_code FROM USERS WHERE user_id=?");
             $query->bind_param("s", $_SESSION["user_id"]);
             $query->execute();
-            $query->bind_result($first_name, $last_name, $email_address, $tel_nr, $address);
+            $query->bind_result($first_name, $last_name, $email_address, $tel_nr, $address_1, $address_2, $city, $postal_code,);
             $query->fetch();
             $query->close();
 
-            drawPageLayout($first_name, $last_name, $tel_nr, $address, $email_address);
+            drawPageLayout($first_name, $last_name, $tel_nr, $address_1, $address_2, $postal_code, $city, $input_email);
         }
 
-        function drawPageLayout($first_name, $last_name, $tel_nr, $address, $email_address){
+        function drawPageLayout($first_name, $last_name, $tel_nr, $address_1, $address_2, $postal_code, $city, $email_address){
             //Draw out the header
             echo "<header>";
             echo "<h1><a href='/index.php'>OFF<span>BRAND</span></a></h1>";
@@ -104,7 +104,25 @@
                 echo "<th>Address</th>";
                 echo "</tr>";
                 echo "<tr>";
-                echo "<th>$address</th>";
+                echo "<th>$address_1</th>";
+                echo "</tr>";
+                echo "<tr>";
+                echo "<th>Care-of-address</th>";
+                echo "</tr>";
+                echo "<tr>";
+                echo "<th>$address_2</th>";
+                echo "</tr>";
+                echo "<tr>";
+                echo "<th>City</th>";
+                echo "</tr>";
+                echo "<tr>";
+                echo "<th>$city</th>";
+                echo "</tr>";
+                echo "<tr>";
+                echo "<th>Postal code</th>";
+                echo "</tr>";
+                echo "<tr>";
+                echo "<th>$postal_code</th>";
                 echo "</tr>";
                 echo "</table>";
                 echo "<div class='user_menu_box'>";
