dd7e50126d52b98ff8a401bfcc8f7038ebc5f880
Emil Nyberg <emilnyberg1998@gmail.com>
2021-11-18 16:57:38 +0100

    trying to fix bug

--- src/Accounts/registration_page.php
+++ src/Accounts/registration_page.php
@@ -8,15 +8,15 @@
 <body>
 <?php
 
-$path = $_SERVER['DOCUMENT_ROOT'];
-$path .= "/database.php";
-include_once($path);
+    //creates connection to database
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/database.php";
+    include_once($path);
 
 // When form submitted, insert values into the database.
 if (isset($_POST['submit'])) {
     //If the user entered the same password twice
     if($_POST["pwd"] == $_POST["pwd2"]){
-        echo"hej";
         $first_name = $_POST['first_name'];
         $last_name  = $_POST['last_name'];
         $email_addres = $_POST['email_address'];
@@ -28,31 +28,31 @@ if (isset($_POST['submit'])) {
         $care_of_address = $_POST["care_of_address"];
         $sha_pwd = sha1($pwd);
     
-            $email_exist = $con->prepare("SELECT email_address FROM USERS WHERE email_address = ?");
-            $email_exist->bind_param("s", $email_addres);
-            $email_exist->execute();
-            $email_exist->bind_result($email_addres_exists);
-            $email_exist->fetch();
-            $email_exist->close();
+        $email_exist = $con->prepare("SELECT email_address FROM USERS WHERE email_address = ?");
+        $email_exist->bind_param("s", $email_addres);
+        $email_exist->execute();
+        $email_exist->bind_result($email_addres_exists);
+        $email_exist->fetch();
+        $email_exist->close();
     
-            $query   = $con->prepare("INSERT INTO USERS (first_name, last_name, email_address, t_number, address_1, pwd, address_2, 
+        $query   = $con->prepare("INSERT INTO USERS (first_name, last_name, email_address, t_number, address_1, pwd, address_2, 
                                     city, postal_code) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"); 
-            $query -> bind_param("sssssssss", $first_name, $last_name, $email_addres, $t_number, $addres, $sha_pwd, $care_of_address, $city, $post_code);
-            $query -> execute();
-            $query->close();
+        $query -> bind_param("sssssssss", $first_name, $last_name, $email_addres, $t_number, $addres, $sha_pwd, $care_of_address, $city, $post_code);
+        $query -> execute();
+        $query->close();
     
-            echo $email_addres_exists;
-            if ($email_addres_exists == "") {  
-                echo "<div class='form'>
-                <h3>User Created Succesfully.</h3><br/>
-                <p class='link'>Click here to <a href='/Accounts/login_page.php'>Log in</a>.</p>
-                </div>";
-            }
-            else{
-                echo "<div class='form'>
-                     <h3>Email already in use.</h3><br/>
-                     <p class='link'>Click here to <a href='registration_page.php'>register</a> again.</p>
-                     </div>";
+        echo $email_addres_exists;
+        if ($email_addres_exists == "") {  
+            echo "<div class='form'>
+            <h3>User Created Succesfully.</h3><br/>
+            <p class='link'>Click here to <a href='/Accounts/login_page.php'>Log in</a>.</p>
+            </div>";
+        }
+        else{
+            echo "<div class='form'>
+            <h3>Email already in use.</h3><br/>
+            <p class='link'>Click here to <a href='registration_page.php'>register</a> again.</p>
+            </div>";
             }
     }else{
         echo "<div class='form'>
@@ -60,7 +60,7 @@ if (isset($_POST['submit'])) {
         <p class='link'>Click here to <a href='registration_page.php'>try again</a></p>
         </div>";
     }
-    } else {
+} else{
 ?>
     <form class="form" action="" method="post">
         <h1 class="login-title">Register</h1>
@@ -83,7 +83,7 @@ if (isset($_POST['submit'])) {
         <label class="password_1">Enter Password</label>
         <input type="password" class="login-input" name="pwd" placeholder="Password" required><br>
         <label class="password_2">Repeat Password</label>
-        <input type="password" class="login-input" name="pwd2" placeholder="Password" required><br>
+        <input type="password" class="login-input" name="pwd2" placeholder="Repeat Password" required><br>
         <input type="submit" name="submit" value="Register" class="login-button">
         <p class="link">Already have an account?<a href="/Accounts/login_page.php"> Click here to Login</a></p>
     </form>
