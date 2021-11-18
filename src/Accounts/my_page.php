f93d0d765efe2e1f76587d35a3741eaea959e8c6
Emil Nyberg <emilnyberg1998@gmail.com>
2021-11-18 15:25:58 +0100

    added some css

--- src/Accounts/my_page.php
+++ src/Accounts/my_page.php
@@ -2,7 +2,7 @@
   <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
     <link rel="stylesheet" href="/Css/my_page.css">
     <title>Offbrand.pwr</title>
   </head>
