e6b869e127d71dd84faf7e8679680ac4ce042fca
ItzCornflakez <dikesalex@gmail.com>
2021-11-19 16:25:33 +0100

    fix css

--- src/Category/gpu.php
+++ src/Category/gpu.php
@@ -6,7 +6,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="Css/main_page.css">
+    <link rel="stylesheet" href="/Css/main_page.css">
     <title>Offbrand.pwr</title>
   </head>
   <body>
