5dd5aaab2805327a2fbb4cdf0e05ea932a51fd6b
Emil Nyberg <emilnyberg1998@gmail.com>
2021-11-20 13:30:36 +0100

    added css to login page and fixed path bug from login page

--- src/Accounts/login_page.php
+++ src/Accounts/login_page.php
@@ -10,11 +10,11 @@
     <form class="login_form" method="post" action="my_page.php">
       <h1 class="form_title">Login</h1>
       <div class="form_elements">
-        <input type="email" id="email" class="login_input" autocomplete="off" required>
+        <input type="email" id="email" name="email" class="login_input" autocomplete="off" required>
         <label for="email" class="form_label">Email</label>
       </div>
       <div class="form_elements">
-        <input type="password" id="password" class="login_input" required>
+        <input type="password" id="password" name="password" class="login_input" required>
         <label for="password" class="form_label">Password</label>
       </div>
         <button class="form_button">Login</button>
