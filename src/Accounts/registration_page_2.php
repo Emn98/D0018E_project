00e4f7e06720bec6996c62eda4caaa43a45438b1
Emil Nyberg <emilnyberg1998@gmail.com>
2021-11-22 00:27:37 +0100

    Added missing bracket

--- src/Accounts/registration_page_2.php
+++ src/Accounts/registration_page_2.php
@@ -6,49 +6,50 @@
 
     // When form submitted, insert values into the database.
     if (isset($_POST['submit'])) {  
-    $first_name = $_POST['first_name'];
-    $last_name  = $_POST['last_name'];
-    $email_addres = $_POST['email_address'];
-    $t_number   = $_POST['phone_nr'];
-    $addres     = $_POST['address'];
-    $pwd        = $_POST['pwd'];
-    $post_code  = $_POST["post_code"];
-    $city       = $_POST["city"];
-    $care_of_address = $_POST["care_of_address"];
-    $sha_pwd = sha1($pwd);
+        $first_name = $_POST['first_name'];
+        $last_name  = $_POST['last_name'];
+        $email_addres = $_POST['email_address'];
+        $t_number   = $_POST['phone_nr'];
+        $addres     = $_POST['address'];
+        $pwd        = $_POST['pwd'];
+        $post_code  = $_POST["post_code"];
+        $city       = $_POST["city"];
+        $care_of_address = $_POST["care_of_address"];
+        $sha_pwd = sha1($pwd);
 
-    //Checks so the user have written the same password twice
-    if($_POST["pwd"] == $_POST["pwd2"]){
-        $email_exist = $con->prepare("SELECT email_address FROM USERS WHERE email_address = ?");
-        $email_exist->bind_param("s", $email_addres);
-        $email_exist->execute();
-        $email_exist->bind_result($email_addres_exists);
-        $email_exist->fetch();
-        $email_exist->close();
+        //Checks so the user have written the same password twice
+        if($_POST["pwd"] == $_POST["pwd2"]){
+            $email_exist = $con->prepare("SELECT email_address FROM USERS WHERE email_address = ?");
+            $email_exist->bind_param("s", $email_addres);
+            $email_exist->execute();
+            $email_exist->bind_result($email_addres_exists);
+            $email_exist->fetch();
+            $email_exist->close();
 
-        $query   = $con->prepare("INSERT INTO USERS (first_name, last_name, email_address, t_number, address_1, pwd, address_2, 
-                        city, postal_code) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"); 
-        $query -> bind_param("sssssssss", $first_name, $last_name, $email_addres, $t_number, $addres, $sha_pwd, $care_of_address, $city, $post_code);
-        $query -> execute();
-        $query->close();
+            $query   = $con->prepare("INSERT INTO USERS (first_name, last_name, email_address, t_number, address_1, pwd, address_2, 
+                                      city, postal_code) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"); 
+            $query -> bind_param("sssssssss", $first_name, $last_name, $email_addres, $t_number, $addres, $sha_pwd, $care_of_address, $city, $post_code);
+            $query -> execute();
+            $query->close();
 
 
-        if ($email_addres_exists == "") {  //If a user with the submitted email already exists
+            if ($email_addres_exists == "") {  //If a user with the submitted email already exists
+                echo "<div class='form'>
+                <h3>User Created Succesfully.</h3><br/>
+                <p class='link'>Click here to <a href='/Accounts/login_page.php'>Log in</a>.</p>
+                </div>";
+            }else{ // Account creation successfull
+                echo "<div class='form'>
+                <h3>Email already in use.</h3><br/>
+                <p class='link'>Click here to <a href='registration_page.php'>register with another email</a>.</p>
+                </div>";
+        }
+        }else{//If the repeated password dosent match the first submitted one
             echo "<div class='form'>
-            <h3>User Created Succesfully.</h3><br/>
-            <p class='link'>Click here to <a href='/Accounts/login_page.php'>Log in</a>.</p>
-            </div>";
-        }else{ // Account creation successfull
-            echo "<div class='form'>
-            <h3>Email already in use.</h3><br/>
-            <p class='link'>Click here to <a href='registration_page.php'>register with another email</a>.</p>
+            <h3>The repeated password differed from the first password</h3><br/>
+            <p class='link'>Click here to <a href='registration_page.php'>try again</a></p>
             </div>";
         }
-    }else{//If the repeated password dosent match the first submitted one
-        echo "<div class='form'>
-        <h3>The repeated password differed from the first password</h3><br/>
-        <p class='link'>Click here to <a href='registration_page.php'>try again</a></p>
-        </div>";
     }
 ?>
 <!DOCTYPE html>
