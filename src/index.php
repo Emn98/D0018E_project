f9d38ac268039368d6c84432277ef66b0afd2cd2
Emil Nyberg <emilnyberg1998@gmail.com>
2021-11-22 18:13:41 +0100

    Added new style to frontpage

--- src/index.php
+++ src/index.php
@@ -12,33 +12,24 @@
     <title>Offbrand.pwr</title>
   </head>
   <body>
-    <div class = "grid-container">
-      <div class = "item1">
-        <h1>OFF<span>BRAND</span></h1>
-        <form class="search_bar_form" method="POST" action="/search.php">
-            <input class="search_bar_inp" type="text" name="product_name">
-            <button type="submit" class="search_btn">Search</button>
-        </form> 
-        <nav>
-          <ul class="menu">
-            <li><a href="/Accounts/site_director.php">My page</a></li>
-            <li><a href="/test.php">Shopping cart</a></li>
-          </ul>
-        </nav>
-      </div>
-      <div class="item2">
-        
-        <!-- This is categorie search -->
-        
-        <ul class="category_list_ul">
+  <div class="container">
+      <header>
+      <h1>OFF<span>BRAND</span></h1>
+      <nav>
+        <ul class="nav_menu">
+          <li><a href="/Accounts/site_director.php">My Page</a></li>
+          <li><a href="#"><i class="fa fa-sign-out"></i>Shopping cart</a></li>
+        </ul>
+      </nav>
+      </header>
+      <main>Main
+          <ul class="category_list_ul">
           <li><a href="/Category/gpu.php">GPU</a></li>
           <li><a href="/Category/cpu.php">CPU</a></li>
         </ul>
-        
-      </div>
-      <div class="item3">
-
-      </div>
-    </div>
+      </main>
+      <div class="left_side">Left Side</div>
+      <div class="right_side">Right Side</div>
+	</div>
   </body>
 </html>
