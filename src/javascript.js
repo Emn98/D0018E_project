0af08cae26eb2cc116ff604d3fac52e6cca8c96e
ItzCornflakez <dikesalex@gmail.com>
2021-11-24 12:02:07 +0100

    fixing

--- src/javascript.js
+++ src/javascript.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
-    $('#click_this').click(function () {
+    $('#category_1').click(function () {
+      var link = $(this).attr('var');
+      $('.post').attr("value",link);
+      $('.redirect').submit();
+    });
+  });
+  $(document).ready(function() {
+    $('#category_2').click(function () {
+      var link = $(this).attr('var');
+      $('.post').attr("value",link);
+      $('.redirect').submit();
+    });
+  });
+  $(document).ready(function() {
+    $('#category_5').click(function () {
       var link = $(this).attr('var');
       $('.post').attr("value",link);
       $('.redirect').submit();
