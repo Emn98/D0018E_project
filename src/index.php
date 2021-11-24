5759f8227cb88f576e94db4680e34bc38e7825f5
ItzCornflakez <dikesalex@gmail.com>
2021-11-24 10:32:37 +0100

    fixing

--- src/index.php
+++ src/index.php
@@ -32,14 +32,14 @@
         
 
         <ul class="category_list_ul">
-          <li><a href='javascript:void(0)' class='list_class' var='http://130.240.200.39/Category/category.php'>GPU</a>
+          <li><a href='javascript:void(0)' id='list_id' var='http://130.240.200.39/Category/category.php'>GPU</a>
           <form method="post" name="redirect" class="redirect">
           <input type="hidden" class="post" name="post" value="1">
           <input type="submit" style="display: none;">
           </form>
-          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" type="text/javascript">
             $(document).ready(function() {
-              $(".list_class").click(function() {
+              $("#list_id").click(function() {
                 alert("Hello, World!");
                 //var link = $(this).attr('var');
                 //$('.post').attr("value",link);
