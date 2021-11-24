20838d6aafac295006cfa82fc4200dcf3d365b42
ItzCornflakez <dikesalex@gmail.com>
2021-11-24 10:33:44 +0100

    fixing

--- src/index.php
+++ src/index.php
@@ -33,10 +33,6 @@
 
         <ul class="category_list_ul">
           <li><a href='javascript:void(0)' id='list_id' var='http://130.240.200.39/Category/category.php'>GPU</a>
-          <form method="post" name="redirect" class="redirect">
-          <input type="hidden" class="post" name="post" value="1">
-          <input type="submit" style="display: none;">
-          </form>
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" type="text/javascript">
             $(document).ready(function() {
               $("#list_id").click(function() {
@@ -47,6 +43,11 @@
               });
             });
           </script>
+          <form method="post" name="redirect" class="redirect">
+          <input type="hidden" class="post" name="post" value="1">
+          <input type="submit" style="display: none;">
+          </form>
+          
           </li>
         </ul>
       </main>
