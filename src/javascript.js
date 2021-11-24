095fc948ab166c5317d6cf45df6b9d5fce479351
ItzCornflakez <dikesalex@gmail.com>
2021-11-24 11:09:02 +0100

    fixing

--- src/javascript.js
+++ src/javascript.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $('#click_this').click(function () {
       alert("Hello, World!");
-      //var link = $(this).attr('var');
-      //$('.post').attr("value",link);
-      //$('.redirect').submit();
+      var link = $(this).attr('var');
+      $('.post').attr("value",link);
+      $('.redirect').submit();
     });
   });
