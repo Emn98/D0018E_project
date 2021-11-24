d126df37050ec7975e24e6bde983f184be0b10a2
ItzCornflakez <dikesalex@gmail.com>
2021-11-24 12:06:26 +0100

    fixing

--- src/Category/category.php
+++ src/Category/category.php
@@ -13,72 +13,77 @@
     <title>Offbrand.pwr</title>
   </head>
   <body>
-    <div class = "grid-container">
-      <div class = "item1">
+    <div class="container">
+      <header>
         <h1>OFF<span>BRAND</span></h1>
         <form class="search_bar_form" method="POST" action="/search.php">
-            <input class="search_bar_inp" type="text" name="product_name">
-            <button type="submit" class="search_btn">Search</button>
+          <input class="search_bar_inp" type="text" name="product_name">
+          <button type="submit" class="search_btn">Search</button>
         </form> 
         <nav>
-          <ul class="menu">
-            <li><a href="/Accounts/site_director.php">My page</a></li>
-            <li><a href="/test.php">Shopping cart</a></li>
+          <ul class="nav_menu">
+            <li><a href="/Accounts/site_director.php">My Page</a></li>
+            <li><a href="/Shopping/shopping_cart.php">Shopping cart</a></li>
           </ul>
         </nav>
-      </div>
-      <div class="item2">     
-      </div>
-      <div class="item3">
-        <?php
+        </header>
+        <main>Main
+          <div class="inner_left_side">
+            <?php include("categories_file.html")?>
+          </div>
+          <div class="inner_right_side">
+          <?php
 
-        $path = $_SERVER['DOCUMENT_ROOT'];
-        $path .= "/database.php";
-        include_once($path);
+            $path = $_SERVER['DOCUMENT_ROOT'];
+            $path .= "/database.php";
+            include_once($path);
 
-        $category_id = (int)$_POST['category_id'];
-        echo ($category_id);
-        
+            $category_id = (int)$_POST['category_id'];
+            echo ($category_id);
 
-        $stmt = $con->prepare("SELECT * FROM PRODUCTS WHERE category_id = ?");
 
-        $stmt->bind_param("i", $category_id);
+            $stmt = $con->prepare("SELECT * FROM PRODUCTS WHERE category_id = ?");
 
-        $stmt->execute();
+            $stmt->bind_param("i", $category_id);
 
-        $result = $stmt->get_result();
+            $stmt->execute();
 
-        if (mysqli_num_rows($result)==0) {
-          echo ("No products matching this category");
-        } else {
+            $result = $stmt->get_result();
 
-        echo "<div class='wrapper'>";
-        while ($row = $result->fetch_assoc()) {
-            $name   = $row['product_name'];
-            $description = $row['product_description'];
-            $category = $row['category_id'];
-            $quantity   = $row['quantity'];
-            $price = $row['price'];
-            $size = $row['size'];
-            $color = $row['color'];
-            $discount = $row['discount'];
-            $picture = $row['picture'];
-            echo "<div class='list_product_div'>";
-            echo "<img src ='$picture' width = '200' height = '250'>";
-            echo "<label class='product_name_label'>$name</label>";
-            echo "<label class='product_name_label'>$description</label>";
-            echo "<form action='/product_details.php' method='post'>";
-            echo "<input type='hidden' name='product_name' value = $name>";
-            echo "<button type='submit' class='product_details_button'>Go to product</button>";
-            echo "</form>";
+            if (mysqli_num_rows($result)==0) {
+              echo ("No products matching this category");
+            } else {
+
+            echo "<div class='wrapper'>";
+            while ($row = $result->fetch_assoc()) {
+                $name   = $row['product_name'];
+                $description = $row['product_description'];
+                $category = $row['category_id'];
+                $quantity   = $row['quantity'];
+                $price = $row['price'];
+                $size = $row['size'];
+                $color = $row['color'];
+                $discount = $row['discount'];
+                $picture = $row['picture'];
+                echo "<div class='list_product_div'>";
+                echo "<img src ='$picture' width = '200' height = '250'>";
+                echo "<label class='product_name_label'>$name</label>";
+                echo "<label class='product_name_label'>$description</label>";
+                echo "<form action='/product_details.php' method='post'>";
+                echo "<input type='hidden' name='product_name' value = $name>";
+                echo "<button type='submit' class='product_details_button'>Go to product</button>";
+                echo "</form>";
+                echo "</div>";
+            }
             echo "</div>";
-        }
-        echo "</div>";
-      }
-        $con->close();
+            }
+            $con->close();
 
-        ?>
-      </div>
+            ?>
+          </div>
+        </main>
+        <div class="left_side">Left Side</div>
+        <div class="right_side">Right Side</div>
     </div>
   </body>
 </html>
