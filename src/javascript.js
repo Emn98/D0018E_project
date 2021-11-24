1fc0de31bbd6d8086ad9f7f4464ad111bf51f53b
ItzCornflakez <dikesalex@gmail.com>
2021-11-24 11:09:56 +0100

    fixing

--- src/javascript.js
+++ src/javascript.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     $('#click_this').click(function () {
-      alert("Hello, World!");
       var link = $(this).attr('var');
       $('.post').attr("value",link);
       $('.redirect').submit();
