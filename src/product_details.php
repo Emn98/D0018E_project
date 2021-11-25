605c96074423ecd9492e8ca5de91ab228edf0dac
Emil Nyberg <emilnyberg1998@gmail.com>
2021-11-25 18:00:50 +0100

    brooo

--- src/product_details.php
+++ src/product_details.php
@@ -106,6 +106,7 @@
                       echo "<option value='$color'>$color</option>";
                     }
                   ?>
+                  </select>
                   <label for='quantity' class='form_label'>Enter Quantity</label>
                   <input type="hidden" class ="purschase_input" name="product_color"  value="<?php echo $picked_color?>">
                     <input type='hidden' id='product_id' name='product_id' class='register_input' value='<?php echo $product_id ?>'>
