c913fe5a0750ca02acf9ffd85dd7fcacd331e21e
ItzCornflakez <dikesalex@gmail.com>
2021-11-25 13:47:20 +0100

    fixing

--- src/search.php
+++ src/search.php
@@ -76,7 +76,7 @@
                   <div class='product_price_buy_div'>
                     <label class='product_price_label'>From <?php echo $price ?> kr</label>
                     <form action='/product_details.php' method='post'>
-                      <input type='hidden' name='product_name' value = $name>
+                      <input type='hidden' name='product_name' value = <?php echo $name ?>>
                       <button type='submit' class='product_details_button'>Go to product</button>
                     </form>
                   </div>
