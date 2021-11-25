1d331bce1426a87e7fa01a7728cf82dff85455f8
ItzCornflakez <dikesalex@gmail.com>
2021-11-25 11:00:16 +0100

    fixing

--- src/product_details.php
+++ src/product_details.php
@@ -85,10 +85,10 @@
             echo "<label class='product_details_price_label'>Current Price: $price</label>";
             echo "<form class='buy_button' method='POST' action='/Shopping/buy.php'>";
             echo "<div class='form_elements'>";
-            echo "<input type='number' id='quantity' name='quantity' class='register_input' placeholder='Quantity' min='0' max=<?php echo '$product_quantity' ?> required> ";
+            echo "<input type='number' id='quantity' name='quantity' class='register_input' placeholder='Quantity' min='0' max=<?php echo $product_quantity ?> required> ";
             echo "<label for='quantity' class='form_label'>Enter Quantity</label>";
             echo "<form action='/Shopping/buy_2.php' method='post'>";
-            echo "<input type='hidden' id='product_id' name='product_id' class='register_input' value=<?php echo '$product_id';?>>";
+            echo "<input type='hidden' id='product_id' name='product_id' class='register_input' value=<?php echo $product_id;?>>";
             echo "<button class='form_button'>Buy</button>";
             echo "</div>";
             echo "<input type='hidden' name='product_id' value ='<?php echo $product_id; ?>'>";
