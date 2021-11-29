450305bc61b9cc12d191883e0103fdb8609665ae
ItzCornflakez <dikesalex@gmail.com>
2021-11-29 13:58:59 +0100

    fixing

--- src/product_handling/check_admin.php
+++ src/product_handling/check_admin.php
@@ -2,6 +2,10 @@
 
 session_start();
 
+if(!isset($_SESSION["user_id"])){
+    header("Location: /Accounts/login_page_form.php");
+    exit;
+}
 
 if( $_SESSION["user_id"] != 0){
     header("Location: /Accounts/user_page.php");
