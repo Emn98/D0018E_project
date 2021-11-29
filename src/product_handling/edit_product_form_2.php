ee5601a10e2d593337f8457d0805c1a9a240810d
ItzCornflakez <dikesalex@gmail.com>
2021-11-29 12:57:37 +0100

    fixing

--- src/product_handling/edit_product_form_2.php
+++ src/product_handling/edit_product_form_2.php
@@ -52,6 +52,8 @@
 
             print_r($_POST);
 
+            $stmt->close();
+
             $stmt = $con->prepare("SELECT * FROM PRODUCT_INVENTORY WHERE product_id = (SELECT product_id FROM PRODUCTS WHERE product_name = ? )");
             $stmt->bind_param("s", $product_name);
             $stmt->execute();
