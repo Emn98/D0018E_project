acc3786a70ba6252249d7558a51c238122eeaf7a
ItzCornflakez <dikesalex@gmail.com>
2021-12-01 15:31:35 +0100

    fixing

--- src/product_handling/edit_product.php
+++ src/product_handling/edit_product.php
@@ -40,7 +40,7 @@ require("check_admin.php");
         // UPDATE PRODUCTS -> UPDATE PRODUCT_INVENTORY -> done
 
         $stmt = $con->prepare("UPDATE PRODUCTS SET product_description=?, category_id=(SELECT category_id FROM CATEGORIES WHERE category_name=?), price=?, size=?, discount=?,
-        picture=? WHERE product_id=(SELECT product_id FROM PRODUCTS WHERE product_name = ?)");
+        picture=? WHERE product_name=?");
         $stmt->bind_param("ssiiiss", $product_description, $category, $price, $size, $discount, $picture, $product_name);
         $stmt->execute();
         $stmt->close();
