99d2484a30b0f9f788704f0cc3187d48704faa70
ItzCornflakez <dikesalex@gmail.com>
2021-12-01 11:39:37 +0100

    fixing

--- src/product_handling/edit_product_form_2.php
+++ src/product_handling/edit_product_form_2.php
@@ -68,7 +68,7 @@ require("check_admin.php");
             echo "<table id='edit_table'>".
                     "<tr><td>Current Colors</td><td>Current Quantity</td></tr>"; 
             while($row_inventory = $result_inventory->fetch_assoc()){
-                echo "<tr><td><input type='text' name='color[]'" . $row_inventory['color'] . "</td><td><input type='text' name='quantity[]'" . $row_inventory['quantity'] . "</td></tr>";
+                echo "<tr><td><input type='text' name='color[]' value=" . $row_inventory['color'] .  "></td><td><input type='text' name='quantity[]' value=".$row_inventory['quantity'] . "></td></tr>";
             }
             echo "</table>"; 
             ?>   
