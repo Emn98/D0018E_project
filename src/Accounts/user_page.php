ddcf9aef12f0ff8a11124aa2175426b9eec2dabf
Emil Nyberg <emilnyberg1998@gmail.com>
2021-12-05 17:13:01 +0100

    Added order history link to user page

--- src/Accounts/user_page.php
+++ src/Accounts/user_page.php
@@ -39,6 +39,7 @@
         <nav>
           <ul class="nav_menu">
             <li><a href="/index.php">Home</a></li>
+            <li><a href="#">Order history</a></li>
             <li><a href="log_out.php"><i class="fa fa-sign-out"></i> Log out</a></li>
           </ul>
         </nav>
