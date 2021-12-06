8be8c337965b927777475fd8b4c02766511b6582
Emil Nyberg <emilnyberg1998@gmail.com>
2021-12-06 03:27:10 +0100

    fixed weird bug in query

--- src/Accounts/delete_user_admin_form.php
+++ src/Accounts/delete_user_admin_form.php
@@ -14,10 +14,10 @@
 
   echo "test2";
 
- $test = 73;
+  $inp = 0;
   
-  $query = $con->prepare("SELECT user_id, email_address, first_name, last_name FROM USERS WHERE user_id=?");
-  $query->bind_param("i", 73);
+  $query = $con->prepare("SELECT user_id, email_address, first_name, last_name FROM USERS WHERE user_id>?");
+  $query->bind_param("i", $inp);
   $query->execute();
   $result = $query->get_result();
   $query->fetch();
