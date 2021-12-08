62542c853f55a0e684f46f85e3232d2737809d40
ItzCornflakez <dikesalex@gmail.com>
2021-12-08 15:20:27 +0100

    fixing

--- src/Product_page/add_review.php
+++ src/Product_page/add_review.php
@@ -8,6 +8,7 @@ $path = $_SERVER['DOCUMENT_ROOT'];
 $path .= "/database.php";
 include_once($path);
 
+$user_id = $_SESSION['user_id'];
 $product_id = $_POST['product_id'];
 $review_name = $_POST['review_name'];
 $review_score = $_POST['review_score'];
@@ -17,7 +18,7 @@ $likes = 0;
 
 $stmt = $con->prepare("INSERT INTO USER_REVIEWS (user_id, product_id, review_name, review_score, review_comment, dislikes, likes, created_at) VALUES
 (?,?,?,?,?,?,?,CURRENT_TIMESTAMP)");
-$stmt->bind_param("iisisii", $SESSION['user_id'], $product_id, $review_name, $review_score, $review_comment, $dislikes, $likes);
+$stmt->bind_param("iisisii", $user_id, $product_id, $review_name, $review_score, $review_comment, $dislikes, $likes);
 $stmt->execute();
 
 $stmt->close();
