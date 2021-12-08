cccd24d9bc505b5cca34b014a753b84d1a07c63a
Emil Nyberg <emilnyberg1998@gmail.com>
2021-12-08 16:37:50 +0100

    javascript test

--- src/Accounts/delete_user_admin_form.php
+++ src/Accounts/delete_user_admin_form.php
@@ -93,7 +93,7 @@
                       echo "<form method='POST' action='delete_user_admin_confirm.php'>";
                       echo gettype($email_address);
                       echo gettype($user_id);
-                      echo "<input type='button' value='Delete' onclick='dosomething(".$email_address.")' class='delete_btn'>";
+                      echo '<input type="button" value="Delete" onclick="dosomething(&quot;div'.$user_id.'&quot;)" class="delete_btn">';
                       echo "</td>";
                       echo "</tr>";
                       $temp = 0;
