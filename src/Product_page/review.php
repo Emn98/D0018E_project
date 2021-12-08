119a99383e36f430be3f46e0935e556d4dac9c76
ItzCornflakez <dikesalex@gmail.com>
2021-12-08 14:11:43 +0100

    fixing

--- src/Product_page/review.php
+++ src/Product_page/review.php
@@ -15,14 +15,26 @@ $stmt->fetch();
 while($review = $review_result->fetch_assoc()){
   $review_name = $review['name'];
   $review_score = $review['review_score'];
+  $review_created_at = $review['created_at'];
   $review_comment = $review['review_comment'];
   $like_to_dislike_ratio = $review['likes'] - $review['dislikes'];
   ?>
-  <div class="each_review_div">
-    <label><?php echo $review_name ?></label><br>
-    <label><?php echo $review_score ?></label><br>
-    <label><?php echo $review_comment ?></label><br>
-    <label><?php echo $like_to_dislike_ratio ?></label><br>
+  <article class="each_review">
+    <header>
+      <div>
+      <label><?php echo $review_name ?></label>
+      <label><?php echo $review_score ?></label>
+      </div>
+      <time datetime="<?php echo $review_created_at ?>"><?php echo $review_created_at ?></time>
+    </header>
+    <p><?php echo $review_comment ?></p>
+    <div>
+      <div>
+        <button>up</button>
+        <label><?php echo $like_to_dislike_ratio ?></label>
+        <button>down</button>
+      </div>
+    </div>
   </div>
  <?php 
 }
