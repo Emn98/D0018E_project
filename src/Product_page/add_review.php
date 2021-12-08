53bbb275f0c78b5a93a41f13c1b37b41881bc6fe
ItzCornflakez <dikesalex@gmail.com>
2021-12-08 18:06:01 +0100

    fixing

--- src/Product_page/add_review.php
+++ src/Product_page/add_review.php
@@ -2,7 +2,20 @@
   session_start();
 ?>
 
+
+
 <?php
+
+function RemoveSpecialChar($str) {
+  
+    // Using str_replace() function 
+    // to replace the word 
+    $res = str_replace( array( '\'', '"',
+    ',' , ';', '<', '>' ), ' ', $str);
+
+    // Returning the result 
+    return $res;
+    }
 
 $path = $_SERVER['DOCUMENT_ROOT'];
 $path .= "/database.php";
@@ -10,9 +23,9 @@ include_once($path);
 
 $user_id = $_SESSION['user_id'];
 $product_id = $_POST['product_id'];
-$review_name = $_POST['review_name'];
-$review_score = $_POST['review_score'];
-$review_comment = $_POST['review_comment'];
+$review_name = RemoveSpecialChar($_POST['review_name']);
+$review_score = RemoveSpecialChar($_POST['review_score']);
+$review_comment = RemoveSpecialChar($_POST['review_comment']);
 $dislikes = 0;
 $likes = 0;
 
