5d52b5b8e792a6b2f671680a90f53cdb65020f91
Emil Nyberg <emilnyberg1998@gmail.com>
2021-12-11 12:44:19 +0100

    Added category add form and styled id

--- src/product_handling/add_category_form.php
+++ src/product_handling/add_category_form.php
@@ -1,25 +1,33 @@
 <?php 
 
   //Checks so the admin is logged in
-  require("admin_check.php");
+  //require("admin_check.php");
 
 ?>
 <!DOCTYPE html>
- <html lang="en">
+<html lang="en">
   <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="/Css/add_category_style.css">
     <title>Add category - Offbrand.pwr</title>
   </head>
   <body>
-    <form class="add_category_form" method="POST" action="add_category.php">
+    <div class="input_container">
+      <form class="add_category_form" method="POST" action="add_category.php">
         <h1>Add New Category</h1>
-        <input type="text" id="category_name" name="category_name" class="category_input" placeholder="Category name" required>
-        <textarea id="category_description" name="category_description" class="category_input" placeholder="Category description" maxlength="255" require>
-        <input type="submit" class="submit_btn" value="Add category">    
-    </form>
-    <form class="cancel_form" method="" action="/Accounts/admin_page.php">
+        <div class="form_elements">
+          <label for="category_name" class="form_label">Name</label>
+          <input type="text" id="category_name" name="category_name" class="category_input" placeholder="Category name" maxlength="20" required>
+        </div>
+        <div class="form_elements">
+          <label for="category_description" class="form_label">Category</label>
+          <textarea id="category_description" name="category_description" class="category_input_text" placeholder="Category description" maxlength="255" require></textarea>
+        </div>
+         <button class="form_button">Submit</button>  
+      </form>
+      <form class="cancel_form" method="" action="/Accounts/admin_page.php">
         <button class="cancel_btn">Cancel</button>
-    </form>
+      </form>
+    </div>
   </body>
 </html>
