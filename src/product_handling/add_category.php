1fdaed03ae10bbbbd635a05e7e2ef3926ac82d2d
Emil Nyberg <emilnyberg1998@gmail.com>
2021-12-11 13:34:50 +0100

    Add category backend should now work

--- src/product_handling/add_category.php
+++ src/product_handling/add_category.php
@@ -20,9 +20,43 @@
   $cat_already_exists->fetch();
   $cat_already_exists->close();
 
-  echo(gettype($category_id));
-
-  if($category_id==NULL){
-      echo "works";
+  if($category_id==NULL){ //The category don't already exists. Create new category
+    $query= $con->prepare("INSERT INTO CATEGORIES (category_name, category_description) VALUES (?, ?)");
+    $query-> bind_param("ss", $cat_name, $cat_description);
+    $query->execute();
+    $query->close();
+    display_creation_success();
+  }else{
+    display_creation_failure();
   }
 ?>
+<!DOCTYPE html>
+ <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/Css/delete_user_response.php">
+    <title>My Page - Offbrand.pwr</title>
+  </head>
+  <body>
+      <?php
+        function display_creation_success(){ //Draw out confirmation that the category have been created  
+      ?>
+        <div class='form'>
+          <h3>Category Created Succesfully.</h3><br/>
+          <p class='link'>Click here to <a href='/product_handling/add_category_form.php'>continue!</a>.</p>
+        </div>
+      <?php 
+        }      
+      ?>
+      <?php
+      function display_creation_failure(){ //Draw out confirmation that the category have been created  
+      ?>
+        <div class='form'>
+          <h3>This Category aldready exists</h3><br/>
+          <p class='link'>Click here to <a href='/product_handling/add_category_form.php'>continue!</a>.</p>
+        </div>
+      <?php 
+      }      
+      ?>
+  </body>
+</html>
