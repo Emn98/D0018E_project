64476dd3ce7df3fd8483cccf5371cf35c83e9861
ItzCornflakez <dikesalex@gmail.com>
2021-12-13 15:17:12 +0100

    fixing

--- src/Product_page/add_review.php
+++ src/Product_page/add_review.php
@@ -28,17 +28,30 @@ $review_score = RemoveSpecialChar($_POST['review_score']);
 $review_comment = RemoveSpecialChar($_POST['review_comment']);
 $dislikes = 0;
 $likes = 0;
+$count = 0;
 
 print_r($_POST);
 echo $user_id;
 
-$stmt = $con->prepare("INSERT INTO USER_REVIEWS (user_id, product_id, review_name, review_score, review_comment, dislikes, likes, created_at) VALUES
-(?,?,?,?,?,?,?,CURRENT_TIMESTAMP)");
-$stmt->bind_param("iisisii", $user_id, $product_id, $review_name, $review_score, $review_comment, $dislikes, $likes);
+$stmt = $con->prepare("SELECT COUNT(*) FROM USER_REVIEWS WHERE user_id=? and product_id=?");
+$stmt->bind_param("ii", $user_id, $product_id);
 $stmt->execute();
-
+$stmt->bind_result($count);
+$stmt->fetch();
 $stmt->close();
 
+if($count == 0){
+  $stmt = $con->prepare("INSERT INTO USER_REVIEWS (user_id, product_id, review_name, review_score, review_comment, dislikes, likes, created_at) VALUES
+  (?,?,?,?,?,?,?,CURRENT_TIMESTAMP)");
+  $stmt->bind_param("iisisii", $user_id, $product_id, $review_name, $review_score, $review_comment, $dislikes, $likes);
+  $stmt->execute();
+  $stmt->close();
+
+} else{
+  ?>
+  <script>alert("already have a review for this product with this user")</script>
+  <?php
+}
 header("Location: /index.php");
 exit;
 ?>
