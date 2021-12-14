7581ed0a4bec9c02fd166bb119608bcd5c8ed196
Emil Nyberg <emilnyberg1998@gmail.com>
2021-12-14 09:49:31 +0100

    new add to cart with quanitty check

--- src/Shopping/add_to_cart.php
+++ src/Shopping/add_to_cart.php
@@ -1,5 +1,4 @@
 <?php
-
   session_start();
 
   //Check so the user have a cart. 
@@ -11,21 +10,71 @@
   $path = $_SERVER['DOCUMENT_ROOT'];
   $path .= "/database.php";
   include_once($path); 
-  
-  $quantity = $_POST['quantity'];
-  $product_id = $_POST['product_id'];
-  $product_color = $_POST['product_color'];
-  $cart_id = $_SESSION["cart_id"];
 
+  //If a user have come from a product_site. 
+  if(isset("quantity") && isset("product_id") && isset("product_color")){
+    $quantity = $_POST['quantity'];
+    $product_id = $_POST['product_id'];
+    $product_color = $_POST['product_color'];
+    $cart_id = $_SESSION["cart_id"];
 
-  //Check the database if product already in cart
-  $query = $con->prepare("SELECT quantity FROM CART_ITEMS WHERE cart_id=? AND product_id=? AND color=?");
-  $query->bind_param("iis", $cart_id, $product_id, $product_color);
-  $query->execute();
-  $query->bind_result($quantity_in_cart);
-  $query->fetch();
-  $query->close();
+    //Check the database if product already in cart
+    $query = $con->prepare("SELECT quantity FROM CART_ITEMS WHERE cart_id=? AND product_id=? AND color=?");
+    $query->bind_param("iis", $cart_id, $product_id, $product_color);
+    $query->execute();
+    $query->bind_result($quantity_in_cart);
+    $query->fetch();
+    $query->close();
 
+    //If the ptroduct is already in user's cart. 
+    if($quantity_in_cart != ""){
+      $quantity = $quantity + $quantity_in_cart;
+
+      $query = $con->prepare("SELECT quantity FROM PRODUCT_INVENTORY WHERE product_id=? and color=?");
+      $query->bind_param("is", $product_id, $product_color);
+      $query->execute();
+      $query->bind_result($quantity_in_stock);
+      $query->fetch();
+      $query->close();
+
+      //Check if there is enough in stock. 
+      if($quantity_in_stock < $quantity){
+        //Relay to the user that the createion was a success
+        echo "<div class='form'>";
+        echo "<h3></h3><br/>";
+        echo "<p class='link'>Click here to <a href='/product_handling/add_category_form.php'>continue!</a>.</p>";
+        echo "</div>";
+        exit;
+      }else{
+        $query = $con->prepare("UPDATE CART_ITEMS SET quantity=? WHERE product_id=? AND cart_id=? AND color=?");
+        $query -> bind_param("iiis", $quantity , $product_id, $cart_id, $product_color);
+        $query -> execute();
+        $query->close();
+      }
+    }else{//If the item don't already exist in user's cart. 
+      $query = $con->prepare("SELECT quantity FROM PRODUCT_INVENTORY WHERE product_id=? and color=?");
+      $query->bind_param("is", $product_id, $product_color);
+      $query->execute();
+      $query->bind_result($quantity_in_stock);
+      $query->fetch();
+      $query->close();
+
+      //Check if there is enough in stock. 
+      if($quantity_in_stock < $quantity){
+        //Relay to the user that the createion was a success
+        echo "<div class='form'>";
+        echo "<h3>Category Created Succesfully.</h3><br/>";
+        echo "<p class='link'>Click here to <a href='/product_handling/add_category_form.php'>continue!</a>.</p>";
+        echo "</div>";
+        exit;
+      }else{
+        $query = $con->prepare("UPDATE CART_ITEMS SET quantity=? WHERE product_id=? AND cart_id=? AND color=?");
+        $query -> bind_param("iiis", $quantity , $product_id, $cart_id, $product_color);
+        $query -> execute();
+        $query->close();
+    }
+    
+  }
   //Retrive the price of the product
   $query = $con->prepare("SELECT price FROM PRODUCTS WHERE product_id=?");
   $query->bind_param("i", $product_id);
@@ -34,33 +83,16 @@
   $query->fetch();
   $query->close();
 
-
-  if($quantity_in_cart != ""){
-    $new_quantity = $quantity + $quantity_in_cart;
-
-    $query = $con->prepare("UPDATE CART_ITEMS SET quantity=? WHERE product_id=? AND cart_id=? AND color=?");
-    $query -> bind_param("iiis", $new_quantity , $product_id, $cart_id, $product_color);
-    $query -> execute();
-    $query->close();
-
- }else{
-    $query = $con->prepare("INSERT INTO CART_ITEMS (cart_id, product_id, quantity, color) 
-                            VALUES(?,?,?,?)"); 
-    $query -> bind_param("iiis",$cart_id, $product_id, $quantity, $product_color);
-    $query -> execute();
-    $query->close();
- }
-
-   $query = $con->prepare("SELECT total_price, total_quantity FROM CARTS WHERE cart_id=?");
-   $query -> bind_param("i",  $cart_id);
-   $query -> execute();
-   $query->bind_result($total_price, $total_quantity);
-   $query->fetch();
-   $query->close();
+  $query = $con->prepare("SELECT total_price, total_quantity FROM CARTS WHERE cart_id=?");
+  $query -> bind_param("i",  $cart_id);
+  $query -> execute();
+  $query->bind_result($total_price, $total_quantity);
+  $query->fetch();
+  $query->close();
 
    
-   $new_total_quantity = $total_quantity + $quantity;
-   $new_total_price = $total_price + ($product_price * $quantity);
+  $new_total_quantity = $total_quantity + $quantity;
+  $new_total_price = $total_price + ($product_price * $quantity);
 
    echo $total_price;
    echo $total_quantity;
@@ -78,8 +110,9 @@
    $query->close();
 
    echo "<div class='form'>
-             <h3>Products Has Been Added To Cart.</h3><br/>
-             <p class='link'>Click here to <a href='/Shopping/shopping_cart.php'>go to Cart</a>.</p>
-             <p class='link'>Click here to <a href='/index.php'>continue shopping</a>.</p>
-             </div>";
+   <h3>Products Has Been Added To Cart.</h3><br/>
+   <p class='link'>Click here to <a href='/Shopping/shopping_cart.php'>go to Cart</a>.</p>
+   <p class='link'>Click here to <a href='/index.php'>continue shopping</a>.</p>
+   </div>";
+  }
 ?>
--- src/Shopping/check_if_user_have_cart.php
+++ src/Shopping/check_if_user_have_cart.php
@@ -1,34 +1,26 @@
 <!-- This php scirpt will check if the logged in user have a cart in the database and
 if not it will create one for him/her -->
 <?php
-
   session_start();
 
-  echo "test 1";
   //Check so the user is logged in.  
   $path = $_SERVER['DOCUMENT_ROOT'];
   $path .= "/Accounts/log_in_check.php";
   include($path);
-
-  echo "test 2";
 
   //creates connection to database
   $path = $_SERVER['DOCUMENT_ROOT'];
   $path .= "/database.php";
   include_once($path);
 
-  echo "test 3";
-
   $user_id = (int) $_SESSION["user_id"];
 
   //Check if the user have a shopping cart in the database
   if(gettype($_SESSION["cart_id"]) == NULL || !isset($_SESSION["cart_id"])){
-    echo "test 4";
     $query= $con->prepare("INSERT INTO CARTS (user_id) VALUES (?)");
     $query-> bind_param("i", $user_id);
     $query->execute();
     $query->close();
-    echo "test 5";
 
     $query = $con->prepare("SELECT cart_id FROM CARTS WHERE user_id = ?");
     $query->bind_param("s", $user_id);
@@ -36,8 +28,6 @@ if not it will create one for him/her -->
     $query->bind_result($cart_id);
     $query->fetch();
     $query->close();
-
-    echo "test 6";
 
     $_SESSION["cart_id"] = $cart_id;
   }
