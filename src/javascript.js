2592d120b5fbc8f9cfd8ef97a2c360578cc50a24
Emil Nyberg <emilnyberg1998@gmail.com>
2021-12-14 10:11:09 +0100

    category_bar_test

--- src/javascript.js
+++ src/javascript.js
@@ -1,25 +1,5 @@
-//listen to category_buttons 
-
-$(document).ready(function() {
-    $('#category_1').click(function () {
-      var link = $(this).attr('var');
-      $('.post').attr("value",link);
-      $('.redirect').submit();
-    });
-  });
-  $(document).ready(function() {
-    $('#category_2').click(function () {
-      var link = $(this).attr('var');
-      $('.post').attr("value",link);
-      $('.redirect').submit();
-    });
-  });
-  $(document).ready(function() {
-    $('#category_5').click(function () {
-      var link = $(this).attr('var');
-      $('.post').attr("value",link);
-      $('.redirect').submit();
-    });
-  });
-
-  
+function go_to_category(id){
+  var category_id = id;
+  $('.post').attr("value",category_id);//Insert the value of the category into the form on line 24. 
+  $('.redirect').submit(); //Submit the form. 
+}
