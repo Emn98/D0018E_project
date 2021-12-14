f4dffbda8ab9c5535cc02381970202002a7ffe55
Emil Nyberg <emilnyberg1998@gmail.com>
2021-12-14 16:41:05 +0100

    Remove button in cart should work

--- src/Shopping/alter_cart_func.php
+++ src/Shopping/alter_cart_func.php
@@ -7,12 +7,16 @@
   $path .= "/database.php";
   include_once($path);
 
+  //If the remove product function have been called. 
   if(isset($_POST["remove"])){
+      $product_id = $_POST["product_id"];
+      $color = $_POST["color"];
+      $cart_id = $_SESSION["cart_id"];
 
-
-
-
-
+      $query = $con->prepare("DELETE FROM CART_ITEMS WHERE cart_id=? AND product_id=? AND color=?");
+      $query->bind_param("i", $cart_id, $product_id, $cart_id);
+      $query->execute();
+      $query->close();
   }
 
 
