2896c11c65048e8dc63daf8e23e7762edb07a485
ItzCornflakez <dikesalex@gmail.com>
2021-12-14 10:32:23 +0100

    fixing

--- src/Product_page/review.php
+++ src/Product_page/review.php
@@ -43,11 +43,11 @@ while($review = $review_result->fetch_assoc()){
   $path .= "/database.php";
   include($path);
 
-  $stmt = $con->prepare("SELECT * FROM USER_COMMENTS WHERE review_id=?");
-  $stmt->bind_param("i", $review_id);
-  $stmt->execute();
-  $comment_result = $stmt->bind_result();
-  $stmt->fetch();
+  $stmt1 = $con->prepare("SELECT * FROM USER_COMMENTS WHERE review_id=?");
+  $stmt1->bind_param("i", $review_id);
+  $stmt1->execute();
+  $comment_result = $stmt1->bind_result();
+  $stmt1->fetch();
   
 
   while($comment = $comment_result->fetch_assoc()){
@@ -75,7 +75,7 @@ while($review = $review_result->fetch_assoc()){
     <?php
 
   }
- 
+  $stmt1->close();
   ?>
   <form class="add_comment_form" method="POST" action="add_comment_form.php">
   <label>If you want to add a comment click here</label>
