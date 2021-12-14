39a7b386bbc4f53de842ae8e0ce3291eaee97e86
Chefenz <pingispontus@hotmail.com>
2021-12-14 09:26:16 +0100

    Added linkt to view_orders

--- src/Accounts/user_page.php
+++ src/Accounts/user_page.php
@@ -38,7 +38,7 @@
         <nav>
           <ul class="nav_menu">
             <li><a href="/index.php">Home</a></li>
-            <li><a href="#">Order history</a></li>
+            <li><a href="/Shopping/view_orders.php">Order history</a></li>
             <li><a href="log_out.php"><i class="fa fa-sign-out"></i> Log out</a></li>
           </ul>
         </nav>
