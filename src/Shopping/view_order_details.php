7f85c741404c019a9cf77f6db26db0e7b910cbd6
Chefenz <pingispontus@hotmail.com>
2021-12-15 14:45:31 +0100

    view_order_details Test HTML 2

--- src/Shopping/view_order_details.php
+++ src/Shopping/view_order_details.php
@@ -10,6 +10,43 @@ $path = $_SERVER['DOCUMENT_ROOT'];
 $path .= "/Accounts/log_in_check.php";
 require($path);
 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/Css/shopping_cart_page.css">
+    <title>Offbrand.pwr</title>
+  </head>
+  <body>
+  <div class="container">
+    <header>
+      <h1>OFF<span>BRAND</span></h1>
+      <nav>
+        <ul class="nav_menu">
+          <li><a href="/index.php"><i class="fa fa-sign-out"></i>Home</a></li>
+          <li><a href="/Accounts/site_director.php">My Page</a></li>
+        </ul>
+      </nav>
+      </header>
+        <div class="shopping_cart">
+          <div class="cart_container">
+            <h2>Orders</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>Product</th>
+                  <th>Color</th>
+                  <th>Quantity</th>
+                  <th>Subtotal</th>
+                </tr>
+              </thead>
+              <tbody>
+              <?php
+
+
 $order_id = $_POST['order_id'];
 
 $query = $con->prepare("SELECT product_id, quantity, color FROM ORDER_ITEMS WHERE order_id=?");
@@ -68,40 +105,5 @@ while ($row = $order_result->fetch_assoc()) {
         $temp = 1;
     }   
 }
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="/Css/shopping_cart_page.css">
-    <title>Offbrand.pwr</title>
-  </head>
-  <body>
-  <div class="container">
-    <header>
-      <h1>OFF<span>BRAND</span></h1>
-      <nav>
-        <ul class="nav_menu">
-          <li><a href="/index.php"><i class="fa fa-sign-out"></i>Home</a></li>
-          <li><a href="/Accounts/site_director.php">My Page</a></li>
-        </ul>
-      </nav>
-      </header>
-        <div class="shopping_cart">
-          <div class="cart_container">
-            <h2>Orders</h2>
-            <table>
-              <thead>
-                <tr>
-                  <th>Product</th>
-                  <th>Color</th>
-                  <th>Quantity</th>
-                  <th>Subtotal</th>
-                </tr>
-              </thead>
-              <tbody>
-              <?php
 
 ?>
