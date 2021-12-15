53d35a0c96e052c6147ae13751ec5eab0e319374
ItzCornflakez <dikesalex@gmail.com>
2021-12-15 13:46:50 +0100

    fixing

--- src/Product_page/review.php
+++ src/Product_page/review.php
@@ -21,22 +21,23 @@ while($review = $review_result->fetch_assoc()){
   $like_to_dislike_ratio = $review['likes'] - $review['dislikes'];
   ?>
   <article class="each_review">
-    <header>
+    <div class="only_review_div">
+      <header>
+        <div>
+        <label><?php echo $review_name ?></label>
+        <label><?php echo $review_score ?></label>
+        </div>
+        <time datetime="<?php echo $review_created_at ?>"><?php echo $review_created_at ?></time>
+      </header>
+      <p><?php echo $review_comment ?></p>
       <div>
-      <label><?php echo $review_name ?></label>
-      <label><?php echo $review_score ?></label>
-      </div>
-      <time datetime="<?php echo $review_created_at ?>"><?php echo $review_created_at ?></time>
-    </header>
-    <p><?php echo $review_comment ?></p>
-    <div>
-      <div>
-        <button>up</button>
-        <label><?php echo $like_to_dislike_ratio ?></label>
-        <button>down</button>
+        <div>
+          <button>up</button>
+          <label><?php echo $like_to_dislike_ratio ?></label>
+          <button>down</button>
+        </div>
       </div>
     </div>
-  
     <?php
     
     $path = $_SERVER['DOCUMENT_ROOT'];
