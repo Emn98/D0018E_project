37f2fedbf278d5f22dbbd5098853e0808d7ae2e1
ItzCornflakez <dikesalex@gmail.com>
2021-12-16 10:03:45 +0100

    fixing

--- src/Product_page/delete_comment.php
+++ src/Product_page/delete_comment.php
@@ -1,23 +1,34 @@
-<?php
-session_start();
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel='stylesheet' href='/Css/delete_user_response.css'>
+      <title>Offbrand.pwr</title>
+    </head>
+    <body>
+        <?php
+        session_start();
 
-//Check so the user is logged in
-require("log_in_check.php");
+        //Check so the user is logged in
+        require("log_in_check.php");
 
-//creates connection to database
-$path = $_SERVER['DOCUMENT_ROOT'];
-$path .= "/database.php";
-include_once($path);
+        //creates connection to database
+        $path = $_SERVER['DOCUMENT_ROOT'];
+        $path .= "/database.php";
+        include_once($path);
 
-$comment_id = $_POST['comment_id'];
+        $comment_id = $_POST['comment_id'];
 
-$stmt = $con->prepare("DELETE * FROM USER_COMMENTS WHERE comment_id=?");
-$stmt->bind_param("i", $comment_id);
-$stmt->execute();
+        $stmt = $con->prepare("DELETE * FROM USER_COMMENTS WHERE comment_id=?");
+        $stmt->bind_param("i", $comment_id);
+        $stmt->execute();
 
-?>
-<div class="form">
-<h3>Comment deleted successfully</h3>
-<br> 
-<a href="/index.php">Click here to continue</a>
-</div>
+        ?>
+        <div class="form">
+        <h3>Comment deleted successfully</h3>
+        <br> 
+        <a href="/index.php">Click here to continue</a>
+        </div>
+    </body>
+  </html>
