f96fa0c67a20925791480806d8b3894a0b81d341
Chefenz <pingispontus@hotmail.com>
2021-12-16 13:16:01 +0100

    bug test

--- src/Shopping/buy_cart.php
+++ src/Shopping/buy_cart.php
@@ -47,6 +47,7 @@ try {
 
       if($new_quantity >= 0){
         update_product_inventory_quantity($product_id, $color);
+        echo"test";
       }else{
         $no_items_in_order = get_amount_of_order_items($order_id);
 
