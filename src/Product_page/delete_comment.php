6e2f5be91648fcde5becdf18b4f71e5ad00ecfd7
ItzCornflakez <dikesalex@gmail.com>
2021-12-16 10:20:18 +0100

    fixing

--- src/Product_page/delete_comment.php
+++ src/Product_page/delete_comment.php
@@ -11,12 +11,12 @@
         session_start();
 
         //Check so the user is logged in
-        require("log_in_check.php");
+        include("log_in_check.php");
 
         //creates connection to database
         $path = $_SERVER['DOCUMENT_ROOT'];
         $path .= "/database.php";
-        include_once($path);
+        include($path);
 
         $comment_id = $_POST['comment_id'];
 
