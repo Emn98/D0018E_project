bafa681fea288feb74fa41383b330a830581764b
Emil Nyberg <emilnyberg1998@gmail.com>
2021-12-17 09:28:07 +0100

    fixed bug

--- src/Accounts/edit_user.php
+++ src/Accounts/edit_user.php
@@ -38,6 +38,13 @@
           $stmt->execute();
           $con->close();
 
+          $hased_new_pwd = sha1($email_address.$_SESSION["user_pwd"]);
+
+          $query = $con->prepare("UPDATE USERS SET pwd=? WHERE user_id=?");
+          $query->bind_param("si", $hased_new_pwd, $_SESSION["user_id"]);
+          $query->execute();
+          $query->close();
+
           echo "<div class='form'>";
           echo "<h3>The information have been successfully changed.</h3>";
           echo "<p class='link'>Click here to <a href='/Accounts/user_page.php'>continue</a>.</p>";
