180bc11366c00d997c315b7dc5cde71fac304361
ItzCornflakez <dikesalex@gmail.com>
2021-12-19 19:46:38 +0100

    fixing

--- src/Product_page/add_like.php
+++ src/Product_page/add_like.php
@@ -22,7 +22,7 @@
         $review_id = $_POST['review_id'];
         $result = 0;
         
-        $stmt = $con->prepare("SELECT COUNT(*) FROM USER_LIKES_REVIEW WHERE user_id = ? AND review_id=?");
+        $stmt = $con->prepare("SELECT COUNT(*) FROM USER_LIKES_REVIEW WHERE user_id=? AND review_id=?");
         $stmt->bind_param("ii", $user_id, $review_id);
         $stmt->execute();
         $stmt->bind_result($result);
@@ -30,7 +30,7 @@
         $stmt->close();
         
         if($result > 0){
-            $stmt = $con->prepare("UPDATE USER_LIKES_REVIEW SET user_liked=? WHERE user_id = ? AND review_id=?");
+            $stmt = $con->prepare("UPDATE USER_LIKES_REVIEW SET user_liked=? WHERE user_id=? AND review_id=?");
             $stmt->bind_param("iii", 1, $user_id, $review_id);
             $stmt->execute();
             $stmt->close(); 
