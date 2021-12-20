1ed127cdd7c234b702aff7ab9c0fe7a225cae105
ItzCornflakez <dikesalex@gmail.com>
2021-12-20 11:26:56 +0100

    fixing

--- src/Product_page/review.php
+++ src/Product_page/review.php
@@ -171,7 +171,6 @@ if($user_id != 0){
                 url:  "add_like.php", // 
                 data: {user_id: u_id, review_id: r_id},                
                 success: function(){
-                  alert("like working");
                   location.reload();
                 },
                 error: function(){
@@ -185,7 +184,6 @@ if($user_id != 0){
                 url:  "add_dislike.php", // 
                 data: {user_id: u_id, review_id: r_id},                
                 success: function(){
-                  alert("dislike working");
                   location.reload();
                 },
                 error: function(){
