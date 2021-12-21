0205053c31cf5af7b13a3b00caf9cecb86e615ff
Chefenz <pingispontus@hotmail.com>
2021-12-21 22:17:16 +0100

    purchase price will now be added to ORDER_ITEMS (untested)

--- src/Shopping/buy_cart_methods.php
+++ src/Shopping/buy_cart_methods.php
@@ -4,7 +4,7 @@
 //$con = mysqli_connect("localhost","phpmyadmin","Offbrand123$","website");
 
 function get_product_info_cart_items($con, $cart_id){
-    $query = $con->prepare("SELECT product_id, quantity, color FROM CART_ITEMS WHERE cart_id=?" );
+    $query = $con->prepare("SELECT product_id, quantity, color, total_price FROM CART_ITEMS WHERE cart_id=?" );
     $query->bind_param("i", $cart_id);
     $query->execute();
     $result = $query->get_result();
@@ -52,6 +52,38 @@ function insert_into_order_items($con, $order_id, $cart_id){
     $query->bind_param("ii", $order_id, $cart_id);
     $query->execute();
     $query->close();
+}
+
+function update_purchase_price_into_ORDER_ITEMS($con, $cart_id, $order_id){
+    $query = $con->prepare("SELECT product_id FROM CART_ITEMS WHERE cart_id=?" );
+    $query->bind_param("i", $cart_id);
+    $query->execute();
+    $result = $query->get_result();
+    $query->fetch();
+    $query->close();
+    $result;
+
+    while($row = $result->fetch_assoc()) {
+
+        $product_id = $row["product_id"];
+
+        $query = $con->prepare("SELECT price, discount FROM PRODUCTS WHERE product_id=?");
+        $query->bind_param("ii", $price, $discount);
+        $query->execute();
+        $query->close();
+
+        if($discount == 0){
+            $stmt = $con->prepare("UPDATE ORDER_ITEMS SET purchase_price=? WHERE order_id=? AND product_id=?");
+            $stmt->bind_param("iis", $price, $order_id, $product_id);
+            $stmt->execute();
+            $stmt->close();
+        }else{
+            $stmt = $con->prepare("UPDATE ORDER_ITEMS SET purchase_price=? WHERE order_id=? AND product_id=?");
+            $stmt->bind_param("iis", $discount, $order_id, $product_id);
+            $stmt->execute();
+            $stmt->close();
+        }
+    }
 }
 
 function delete_from_cart_items($con, $cart_id){
