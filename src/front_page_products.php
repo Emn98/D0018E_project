a28d0d989bc8fd38daa5baec2c505da1a9dccf29
Emil Nyberg <emilnyberg1998@gmail.com>
2021-12-21 11:51:20 +0100

    Changed style

--- src/front_page_products.php
+++ src/front_page_products.php
@@ -63,8 +63,10 @@ $query->close();
         $average_score=$row["average_score"];
         ?>
         <div class="card">
-            <p class="show_score">Score:<?php echo $average_score;?>/5</p>
-            <img src="<?php echo $img; ?>" width="170" height="200">
+            <div class="img_cont">
+              <p class="show_score">Score:<?php echo $average_score;?>/5</p>
+              <img src="<?php echo $img; ?>" width="170" height="200">
+            </div>
             <h2><?php echo $product_name; ?></h2>
             <p class="description"><?php echo $product_description; ?></p>
             <?php 
