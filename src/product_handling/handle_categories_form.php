62e13e981bed44b0ed96efcd508049241a64cb42
Emil Nyberg <emilnyberg1998@gmail.com>
2021-12-21 08:56:33 +0100

    handle category form for admin

--- src/product_handling/handle_categories_form.php
+++ src/product_handling/handle_categories_form.php
@@ -10,6 +10,7 @@ include($path);
 $stmt = $con->prepare("SELECT * FROM CATEGORIES");
 $stmt->execute();
 $result = $stmt->get_result();
+$result2 = $stmt->get_result();
 ?>
 <!DOCTYPE html5>
 <html>
@@ -23,7 +24,7 @@ $result = $stmt->get_result();
       <form class="category_handle" method="POST" action="handle_categories.php">
         <h2>Remove category</h2>    
         <label for="category_to_delete">Choose category to remove</label>
-        <select name="category_to_delete">
+        <select name="category_to_delete">Delete category
           <?php
           while($row = $result->fetch_assoc()){
             $category_name = $row['category_name'];
@@ -39,11 +40,15 @@ $result = $stmt->get_result();
       <form class="category_handle" method="POST" action="handle_categories.php">
         <h2>Reinstate deleted category</h2>
         <label for="category_to_reinstate">Choose categort to reinstate</label>
-        <select name="category_to_reinstate">
+        <select name="category_to_reinstate">Reinstate category
           <?php
-          function deleted($category_name){
-            echo "<option value='$category_name'>$category_name</option>";   
-          } 
+          while($row = $result2->fetch_assoc()){
+            $category_name = $row['category_name'];
+            $is_deleted    = $row['is_deleted'];    
+            if($is_deleted==1){
+              echo "<option value='$category_name'>$category_name</option>";
+            }  
+          }
           ?>    
         </select>
       </form>    
