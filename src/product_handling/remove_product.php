25137aa85cd89bacdbd4989df660a77bd1674f0c
Emil Nyberg <emilnyberg1998@gmail.com>
2022-01-04 16:19:43 +0100

    changed remove product

--- src/product_handling/remove_product.php
+++ src/product_handling/remove_product.php
@@ -1,49 +1,43 @@
 <?php
-require("check_admin.php");
+
+if(isset($_POST["remove"]) && $_POST["category_to_delete"]!=""){
+  $category_name = $_POST["category_to_delete"];
+
+  //Establish connection to database. 
+  $path = $_SERVER['DOCUMENT_ROOT'];
+  $path .= "/database.php";
+  include($path);
+
+  $inp = 1;
+  
+  $query = $con->prepare("UPDATE PRODUCTS SET is_deleted=? WHERE product_name=?");
+  $query -> bind_param("is",$inp, $category_name);
+  $query -> execute();
+  $query->close();
+
+  header("Location: remove_product_form.php");
+  exit;
+}
+
+if(isset($_POST["reinstate"]) && $_POST["category_to_reinstate"]!=""){
+    $category_name = $_POST["category_to_reinstate"];
+
+    //Establish connection to database. 
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/database.php";
+    include($path);
+  
+    $inp = 0;
+    
+    $query = $con->prepare("UPDATE PRODUCTS SET is_deleted=? WHERE product_name=?");
+    $query -> bind_param("is",$inp, $category_name);
+    $query -> execute();
+    $query->close();
+  
+    header("Location: remove_product_form.php");
+    exit;
+  }
+
+header("Location: remove_product_form.php");
+exit;  
 ?>
-
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="/Css/product.css">
-
-    <title>Remove Product</title>  
-  </head>
-  <body>
-      
-      <?php
-
-        $product_name = $_POST['product_name'];
-        
-        $path = $_SERVER['DOCUMENT_ROOT'];
-        $path .= "/database.php";
-        include($path);
-
-        $stmt = $con->prepare("SELECT product_id FROM PRODUCTS WHERE product_name=?");
-
-        $stmt->bind_param("s", $product_name);
-        $stmt->execute();
-        $stmt->bind_result($product_id);
-        $stmt->fetch();     
-        $stmt->close();
-     
-        $stmt = $con->prepare("DELETE FROM PRODUCT_INVENTORY WHERE product_id=?");
-
-        $stmt->bind_param("i", $product_id);
-        $stmt->execute();
-        $stmt->close();
-        
-        $stmt = $con->prepare("DELETE FROM PRODUCTS WHERE product_id=?");
-        
-        $stmt->bind_param("i", $product_id);
-        $stmt->execute();
-        $stmt->close();
-      ?>
-
-    <form action="remove_product_form.php" method="post">
-    <button type="submit" class="btn">Return</button>
-    </form>
-
-  </body>
-</html>
--- src/product_handling/remove_product_form.php
+++ src/product_handling/remove_product_form.php
@@ -1,51 +1,62 @@
 <?php
+//Checks so the admin is logged in. 
 require("check_admin.php");
-?>
 
-<html lang="en">
+//Establish connection to database. 
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/database.php";
+include($path);
+
+$stmt = $con->prepare("SELECT * FROM PRODUCTS");
+$stmt->execute();
+$result = $stmt->get_result();
+$deleted_categories=array();
+?>
+<!DOCTYPE html5>
+<html>
   <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="/Css/product.css">
-      <title>Remove Product</title>
-
-    
+    <link rel="stylesheet" href="/Css/handle_category_style.css">
+    <title>Remove item - Offbrand.pwr</title>
   </head>
   <body>
-    <div class="product_div">
-      <div class="inner_product_div">
-        <h1>Remove Product Page</h1>
-   
-        <form action="remove_product.php" method="post">
-        <label for="product_name">Name</label>
-        <select name="product_name" id="product_name">
-        <?php
-
-          $path = $_SERVER['DOCUMENT_ROOT'];
-          $path .= "/database.php";
-          include_once($path);
-
-          $stmt = $con->prepare("SELECT * FROM PRODUCTS");
-          
-          $stmt->execute();
-
-          $result = $stmt->get_result();
- 
+    <div class="container">
+      <form class="category_handle" method="POST" action="remove_product.php">
+        <h2>Remove item</h2>    
+        <label for="category_to_delete">Choose item to remove</label>
+        <select name="category_to_delete">Delete item
+          <?php
           while($row = $result->fetch_assoc()){
-            $product_name = $row['product_name'];
-            echo "<option value='$product_name'>$product_name</option>";
+            $category_name = $row['product_name'];
+            $is_deleted    = $row['is_deleted'];    
+            if($is_deleted==0){
+              echo "<option value='$category_name'>$category_name</option>";
+            }else{
+              array_push($deleted_categories,$category_name);
+            }  
           }
-          $con->close();
-          
-         ?>
-         </select><br>
-        <button type="submit" class="btn">Send</button>
-        </form>
-
-        <form action="/Accounts/admin_page.php" method="post">
-          <button type="submit" class="btn">Return</button>
-        </form>
-      </div>
+          ?>    
+        </select>
+        <input type="hidden" value="delete" name="remove">
+        <button type="submit" class="submit_btn">Submit</button>
+      </form>
+      <br>
+      <form class="category_handle" method="POST" action="remove_product.php">
+        <h2>Reinstate deleted item</h2>
+        <label for="category_to_reinstate">Choose item to reinstate</label>
+        <select name="category_to_reinstate">Reinstate item
+          <?php
+          foreach ($deleted_categories as $value) {
+            echo "<option value='$value'>$value</option>";
+          }
+          ?>    
+        </select>
+        <input type="hidden" name="reinstate" value="reinstate">
+        <button type="submit" class="submit_btn">Submit</button>
+      </form>
+      <form class="go_back_form" method="" action="/Accounts/admin_page.php">
+        <button class="back_btn" type="submit">Go Back</button>    
+      </form>    
     </div>
-  </body>
+  </body>    
 </html>
